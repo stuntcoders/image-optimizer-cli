2ca7fd3689572010bcb80dd9a84ee8052c24cff8
Mladen Ilic <mladen@stuntcoders.com>
2017-12-26 21:16:43 +0100

    Move local modules under lib directory

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 global.argv = require('minimist')(process.argv.slice(2));
 
-const crawl = require('./crawl');
-const optimize = require('./optimize');
-const log = require('./log')(argv.log);
+const crawl = require('./lib/crawl');
+const optimize = require('./lib/optimize');
+const log = require('./lib/log')(argv.log);
 
 crawl(argv.path, file => optimize(file)
   .then(result => log(file, result))
