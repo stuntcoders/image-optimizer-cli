50739b4801c3e5aa9c26ae9e785f77b97ba71583
Mladen Ilic <mladen@stuntcoders.com>
2018-02-27 20:19:54 +0100

    Render usage info

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ global.argv = require('minimist')(process.argv.slice(2));
 const crawl = require('./lib/crawl');
 const optimize = require('./lib/optimize');
 const log = require('./lib/log')(argv.log);
+const usage = require('./lib/helper/usage');
 
-crawl(argv.path, file => optimize(file)
+const path = argv._[0];
+
+if (!path) {
+  return usage();
+}
+
+crawl(path, file => optimize(file)
   .then(result => log(file, result))
   .catch(error => console.error(error))
 );
